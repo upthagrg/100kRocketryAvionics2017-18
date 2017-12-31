c538a50b36f7875d93494301ae29019f4341c9ce
upthagrg <upthagrg@oregonstate.edu>
2017-12-31 11:58:36 -0800

    added install script

--- logging/logger.c
+++ logging/logger.c
@@ -1,12 +1,8 @@
 /*******************************************************************************************
-*Title: datagen.c
+*Title: logger.c
 *Author: Glenn Upthagrove
-*Date: 12/20/2017
-*Description: A semi-random generator of JSON formatted data for testing other pieces of the 
-*tracking software. The altitude is a simple quadratic, and the velocity is the derivative
-*thereof. The rate of update can be set via -rate, the initial altitude via -alt, the 
-*initial latitude via -lat, the initial longitude via -lon, and the initial velocity via 
-*-vel. If any of thee are not set, defaults are used.
+*Date: 12/28/2017
+*Description: A grogram that logs incoming telemetry data to a file. 
 *******************************************************************************************/
 #define _XOPEN_SOURCE 500 //makes usleep work
 #include <stdio.h>
@@ -15,26 +11,31 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+float freq;
+float alt;
+float vel; 
+float lat;
+float lon;
+float Time1;
 
-void gen_data(float freq, float salt, float svel, float slat, float slon){
+void gen_data2(char* buff, float svel, float salt, float slat, float slon){
 	char str1[9] = "velocity";
 	char str2[9] = "latitude";
 	char str3[10] = "longitude";
 	char str4[9] = "altitude";
 	char str5[5] = "time";
-	char buff[256];
-	float vel=svel; //initial velocity, no acceleration other than gravity, no jerk
-	float lat=slat;
-	float lon=slon;
-	float alt=salt;
-	float time=0.0;
+//	char* buff;
 	int dec;
-	do{
+	//	buff = (char*)malloc(sizeof(char)*256);
 		memset(buff, '\0', 256);
-		sprintf(buff,"'{\"%s\":\"%f\", \"%s\":\"%f\", \"%s\":\"%f\", \"%s\":\"%f\", \"%s\":\"+%f\"}'", str1,vel,str2,lat,str3,lon,str4,alt,str5,time); //make JSON string
-		printf("%s\n", buff); //print JSON string
-		time += (1.0/freq); //update time
-		alt = ((-4.9*powf(time, 2)) + (svel*time) + salt); //update alt
+		sprintf(buff,"'{\"%s\":\"%f\", \"%s\":\"%f\", \"%s\":\"%f\", \"%s\":\"%f\", \"%s\":\"+%f\"}'", str1,vel,str2,lat,str3,lon,str4,alt,str5,Time1); //make JSON string
+		//printf("%s\n", buff); //print JSON string
+	//	printf("time: %f\n", Time1);
+		Time1 += (1.0/freq); //update time
+	//	printf("time: %f\n", Time1);
+	//	printf("alt: %f\n", alt);
+		alt = ((-4.9*powf(Time1, 2)) + (svel*Time1) + salt); //update alt
+	//	printf("alt: %f\n", alt);
 		dec = rand()%2;
 		if(dec){ //update lat
 			lat += 0.001;
@@ -49,20 +50,30 @@ void gen_data(float freq, float salt, float svel, float slat, float slon){
 		else{
 			lon -= 0.001;
 		}
-		vel = ((-9.8*time) + (svel)); //update vel
-		usleep(1000000/freq);
-	}while(alt>salt);
+		vel = ((-9.8*Time1) + (svel)); //update vel
+		//usleep(1000000/freq);
+	//	return buff; //return JSON string
 	
 }
 
 int main(int argc, char** argv){
-	float freq = 1;
+	//freq = 1;
+	int i=0;
+	char* str;
 	float salt = 0.0;
 	float svel = 100.0;
 	float slat = 45.0;
 	float slon = 45.0;
-	int i=0;
+	Time1 = 0.0;
+	vel = svel;
+	lat = slat;
+	lon = slon;
+	alt = salt;
+	freq = 1;
+	
 	srand(time(NULL));
+	str = (char*)malloc(sizeof(char)*256);
+
 	for(i; i<argc; i++){
 		if(strcmp(argv[i], "-vel") == 0){ //get starting velocity
 			svel = atof(argv[i+1]);
@@ -80,6 +91,10 @@ int main(int argc, char** argv){
 			freq = atof(argv[i+1]);
 		}
 	}
-	gen_data(freq, salt, svel, slat, slon);
+	do{
+		gen_data2(str, svel, salt, slon, slat);
+		printf("%s\n", str);
+		usleep(1000000/freq);
+	}while(alt>=salt);
 	return 0;
 }
