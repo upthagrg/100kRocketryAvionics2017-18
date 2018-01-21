ee457ef8724e3d19483f599c9aaf8c574c0eb1b9
Sam Hudson <Sam_Hudson@symantec.com>
2018-01-20 18:26:56 -0800

    added api calls to data handler

--- data_handling/handle.py
+++ data_handling/handle.py
@@ -1,8 +1,14 @@
+import requests, json
+
 done = False
 while done == False:
 	with open('./commfifo') as fifo:
-		data = fifo.read()
-	print data
-	if data.find("**&&") != -1:
+	    data = fifo.read()
+        jdata = json.loads(data)
+        print jdata
+        fifo.close()
+        r = requests.post('http://localhost:5000/api/v1.0/telemetry', json = jdata)
+        print r.text
+        if data.find("**&&") != -1:
 		done = True
 	fifo.close()
