4fc784e3c0580b7d3ea22945313626185894ebf1
Sam Hudson <s@mhudson.me>
2018-01-28 23:09:08 -0800

    updates

--- web_tools/api/app.py
+++ web_tools/api/app.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask, redirect, url_for, request, render_template, jsonify
+import pymongo
 from pymongo import MongoClient
+from flask_cors import CORS
+import json
+import hashlib
 
 app = Flask(__name__)
-
+CORS(app)
 client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
 db = client.telemetrydb
 
 
-@app.route('/api/v1.0/telemetry', methods=['GET'])
+@app.route('/api/v1.0/telemetry_all', methods=['GET'])
 def get_all_telemetry():
     telemetry = db.telemetrydb
     output = []
@@ -22,12 +26,28 @@ def get_all_telemetry():
         })
     return jsonify({'result': output})
 
+@app.route('/api/v1.0/telemetry', methods=['GET'])
+def get_telemetry():
+    telemetry = db.telemetrydb
+    output = []
+    cursor = telemetry.find().sort([("time", pymongo.DESCENDING)])
+    output.append({
+        'altitude': cursor[0]['altitude'],
+        'longitude': cursor[0]['longitude'],
+        'latitude': cursor[0]['latitude'],
+        'time': cursor[0]['time'],
+        'velocity': cursor[0]['velocity']
+    })
+    return jsonify({'result': output})
+
 
 @app.route('/api/v1.0/telemetry', methods=['POST'])
 def post_telemetry():
     _telemetry = request.json
     db.telemetrydb.insert_one(_telemetry)
-    return "Successfully added"
+    hash_object = hashlib.md5(str(_telemetry))
+    hash_object.hexdigest()
+    return "Success:" + hash_object.hexdigest()
 
 
 if __name__ == "__main__":
--- web_tools/app/app.py
+++ web_tools/app/app.py
@@ -8,11 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    output = "hello"
     r = requests.get('http://172.17.0.4:5000/api/v1.0/telemetry')
-    output = json.dumps(r.text)
-    return render_template("index.html",output=output)
+    telemetry = json.loads(r.text)['result']
+    return render_template("index.html", telemetry=telemetry)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
