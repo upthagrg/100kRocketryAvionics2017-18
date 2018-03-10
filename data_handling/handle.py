bc098665c76813fe66cf5d637b3d4671fbd98130
upthagrg <upthagrg@oregonstate.edu>
2018-03-10 14:16:44 -0800

    updated handle.py for double data

--- data_handling/handle.py
+++ data_handling/handle.py
@@ -2,17 +2,34 @@ import requests, json
 launch = requests.get('http://localhost:5000/api/v1.0/prepare_launch')
 r = requests.get('http://localhost:5000/api/v1.0/start_launch/'+launch.text)
 print r.text
+flag1 = True;
+flag2 = True;
 while 1:
-    with open('./commfifo') as fifo:
-        data = fifo.read()
-    if data == "**&&":
-        print "End of transmission"
-        break
-    jdata = json.loads(data)
-    print jdata
-    fifo.close()
-    r = requests.post('http://localhost:5000/api/v1.0/telemetry', json = jdata)
-    print r.text
-    fifo.close()
+    if flag1 == True:
+      with open('./commfifo1') as fifo1: # booster data
+          data1 = fifo1.read()
+    if flag2 == True
+      with open('./commfifo2') as fifo2: # sustainer data
+          data2 = fifo2.read()
+    if data1 == "**&&":
+        print "End of booster transmission"
+	flag1 = False
+        #break
+    if data2 == "**&&":
+        print "End of sustainer transmission"
+	flag2 = False
+        #break
+    if flag1 == True:
+    	jdata = json.loads(data1)
+    	print jdata
+    	r = requests.post('http://localhost:5000/api/v1.0/telemetry', json = jdata)
+    	print r.text
+    	fifo1.close()
+    if flag2 == True:
+    	jdata = json.loads(data2)
+    	print jdata
+    	r = requests.post('http://localhost:5000/api/v1.0/telemetry', json = jdata)
+    	print r.text
+    	fifo2.close()
 r = requests.get('http://localhost:5000/api/v1.0/end_launch/'+launch.text)
 print r.text
