54d462e681725f340437cc80566073f812ef646d
atermonera <atermonera@gmail.com>
2018-03-12 19:20:11 -0700

    Compiles and runs

--- onboard/tests/matrix_test.h
+++ onboard/tests/matrix_test.h
@@ -1,18 +1,18 @@
 #ifndef MATRIX_TEST_H
 #define MATRIX_TEST_H
 
-void matrix_init_test(void);
-void matrix_element_get_test(void);
-void matrix_element_set_test(void);
-void matrix_identity_test(void);
-void matrix_row_vector_test(void);
-void matrix_column_vector_test(void);
-void matrix_row_add_test(void);
-void matrix_row_scale_test(void);
-void matrix_scale_test(void);
-void matrix_transpose_test(void);
-void matrix_add_test(void);
-void matrix_multiply_test(void);
-void matrix_inverse_test(void);
+int8_t matrix_init_test(void);
+int8_t matrix_element_get_test(void);
+int8_t matrix_element_set_test(void);
+int8_t matrix_identity_test(void);
+int8_t matrix_row_vector_test(void);
+int8_t matrix_column_vector_test(void);
+int8_t matrix_row_add_test(void);
+int8_t matrix_row_scale_test(void);
+int8_t matrix_scale_test(void);
+int8_t matrix_transpose_test(void);
+int8_t matrix_add_test(void);
+int8_t matrix_multiply_test(void);
+int8_t matrix_inverse_test(void);
 
 #endif
--- onboard/tests/vector_test.h
+++ onboard/tests/vector_test.h
@@ -1,7 +1,7 @@
 #ifndef VECTOR_TEST_H
 #define VECTOR_TEST_H
 
-void vector_init_test(void);
-void vector_dot_product_test(void);
+int8_t vector_init_test(void);
+int8_t vector_dot_product_test(void);
 
 #endif
