a41e20dbd13a8631f5471eb18655d60c36be6fd1
upthagrg <upthagrg@oregonstate.edu>
2018-04-24 09:28:11 -0700

    work on Teledongle interface

--- data_handling/telemega_retrieve.py
+++ data_handling/telemega_retrieve.py
@@ -1,14 +1,15 @@
+#interface to Teledongle over USB. This uses the pyUSB module and its utility library.
 import usb.core
 import usb.util
 import sys
 
-#deines
-altus = 0xfffe
-teledongle = 0x000c
-config = 1
-intf = 0
-alternate = 0
-ep = 1
+#defines
+altus = 0xfffe #altus metrum vendor id
+teledongle = 0x000c #Teledongle devic id
+config = 1 #configuration
+intf = 0 #interface
+alternate = 0 #alternate 
+ep = 1 #endpoint
 
 #find teledongle
 device = usb.core.find(idVendor=altus, idProduct=teledongle)
@@ -26,7 +27,7 @@ if device.is_kernel_driver_active(0):
 	device.detach_kernel_driver(0)
 
 #set configuration to first found 
-#device.set_configuration()
+#device.set_configuration() #I have yet to have this work with good results
 
 #get endpoint
 configuration = device.get_active_configuration()
@@ -39,8 +40,22 @@ assert endpoint is not None
 
 print "Telemega configured successfully"
 
-#msg = 'test'
-#assert len(device.write(1, msg, 100)) == len(msg)
-#ret = device.read(0x81, len(msg), 100)
-#sret = ''.join([chr(x) for x in ret])
-#assert sret == msg
+#Teledongle params
+freq = 'c F 434550'
+data_rate = 'c T 0'
+listen = 'm 20'
+
+#write params
+device.write(endpoint, freq, 100)
+device.write(endpoint, data_rate, 100)
+device.write(endpoint, listen, 100)
+
+#data buffer
+buff = ''
+
+#start indefintie loop for listening to Teledongle
+while(True):
+	token = device.read(endpoint, 1, 100)
+	buff += token
+	print(token)
+	print(buff)
